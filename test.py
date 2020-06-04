2726c62dbed21b82ca6de9ff0293d6cfe396889d
Jason Shen <syj0614@gmail.com>
2020-06-03 23:09:08 -0700

    d

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+# from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+# from sage.rings.rational_field import QQ
+# from sage.rings.ideal import Ideal
+# from sage.symbolic.ring import var
+from sage.all import *
+
 variables = ["x", "y"]
 ring = PolynomialRing(QQ, variables)
