d372160d7f8c9db31ae5c6e3cdd5978c2a4b713f
Jason Shen <syj0614@gmail.com>
2020-06-03 23:52:52 -0700

    d

--- organic.py
+++ organic.py
@@ -5,7 +5,6 @@ with open (sys.argv[1], "r") as fp:
     graphs = json.load(fp)["graphs"]
 
 n_graphs = len(graphs)
-results = []
 for g_id, g in enumerate(graphs):
     print ("Handeling {} / {} graphs".format(g_id, n_graphs))
     if len(g["arrow_edges"]) == 0 and len(g["latent_edges"]) == 0:
@@ -13,9 +12,15 @@ for g_id, g in enumerate(graphs):
         continue
     l = lr.Linear(g["graph_str"])
     print (g["graph_str"])
+    g_res = {}
     for edge in g["arrow_edges"]:
         basis, target, e = l.gidentify((edge[0], edge[1]))
+        g_res[(edge[0], edge[1])] = (basis, target, e)
         if len(basis) > 0:
-            print (basis[-1].roots())
-            import sys
-            sys.exit(1)
+            assert(basis[-1].degree(target) > 0)
+    for edge in g["latent_edges"]:
+        l_edge = tuple(["latent"] + sorted(edge))
+        basis, target, e = l.gidentify(l_edge)
+        g_res[l_edge] = (basis, target, e)
+        if len(basis) > 0:
+            assert(basis[-1].degree(target) > 0)
