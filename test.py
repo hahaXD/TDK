b41ab880ccaa6f7455d799d1209c2e07ea1fd834
Jason Shen <syj0614@gmail.com>
2020-06-04 02:31:23 -0700

    test

--- test.py
+++ test.py
@@ -2,14 +2,20 @@ import pickle
 import sys
 from sage.all import *
 import glob
-
-fnames = glob.glob("usda/*.pk")
-for fname in fnames:
-    with open(fname, "rb") as fp:
-        result = pickle.load(fp)
-        for key in result:
-            print (key, len(result[key][0]), result[key][0])
-            if len(result[key][0]) > 1:
-                break
+summary = {}
+for i in range(1, 4096):
+    fnames = glob.glob("usda/{}_*.pk".format(i))
+    for fname in fnames:
+        with open(fname, "rb") as fp:
+            result = pickle.load(fp)
+            for key in result:
+                result[key][0].degree(result[key][1])
+                dg = result[key][0][0].degree(result[key][1])
+                if dg not in summary:
+                    if dg not in summary:
+                        summary[dg] = 1
+                    else:
+                        summary[dg] += 1
+print (summary)
 
 
