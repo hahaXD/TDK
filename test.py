18cd11ee169c20475848f41bc429ca73806edde4
Jason Shen <syj0614@gmail.com>
2020-06-04 02:42:45 -0700

    test

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ from sage.all import *
 import glob
 summary = {}
 for i in range(1, 4096):
-    fnames = glob.glob("usda/{}_*.pk".format(i))
+    fnames = glob.glob("usda/{}_*.pickle".format(i))
     for fname in fnames:
+        print (fname)
         with open(fname, "rb") as fp:
             result = pickle.load(fp)
             for key in result:
                 result[key][0].degree(result[key][1])
                 dg = result[key][0][0].degree(result[key][1])
-                print (dg)
                 if dg not in summary:
                     summary[dg] = 1
                 else:
